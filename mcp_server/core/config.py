4645fa0b35e9dc55cf9476d1e3d7c47f475c8eb6
Aan <aanshik2004@gmail.com>
2025-07-12 15:05:04 +0530

    chore: Move git folder to root directory

--- mcp_server/core/config.py
+++ mcp_server/core/config.py
@@ -7,13 +7,13 @@ class Settings(BaseSettings):
     from .env file or system environment variables.
     """
 
-    DATABASE_URL: str = "postgresql+asyncpg://unisql_user:unisql_user@localhost:5432/unisqldb"
+    DATABASE_URL: str = "postgresql+asyncpg://movie_user:movie_user@localhost:5432/movies"
 
-    DB_USER: str = "unisql_user"
-    DB_PASSWORD: str = "unisql_user"
-    DB_NAME: str = "unisqldb"
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 5432
+    # DB_USER: str = "unisql_user"
+    # DB_PASSWORD: str = "unisql_user"
+    # DB_NAME: str = "unisqldb"
+    # DB_HOST: str = "localhost"
+    # DB_PORT: int = 5432
     SERVER_TRANSPORT: str = "stdio"
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
